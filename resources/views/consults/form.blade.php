93d627143495585c71cc57b35b943830c4af7f6f
emjay97 <mjayestilles@gmail.com>
2020-01-08 10:33:06 +0800

    Edited Consults form.

--- resources/views/consults/form.blade.php
+++ resources/views/consults/form.blade.php
@@ -1,5 +1,14 @@
-<!-- Con Datesxstart Field -->
+<div class="form-group col-sm-12">
+  <label for="patients_id">Patient Name: </label>
+  <select name="patients_id" id="patients_id" class="form-control selectpicker"  data-live-search="true" data-live-search-placeholder="Select Patient" title="Select Patient">
+      <option value=""> Select Patient</option>
+    @foreach($patients as $patient)
+      <option value="{{$patient->id}}"}}> {{$patient->px_name}}</option>
+    @endforeach
+  </select>
+</div> 
 
+<!-- Con Datesxstart Field -->
 <div class="form-group col-sm-12">
     {!! Form::label('con_dateSxStart', 'Date:') !!}
     {!! Form::date('con_dateSxStart', null, ['class' => 'form-control','id'=>'con_dateSxStart', 'required']) !!}
@@ -13,17 +22,7 @@
         })
     </script>
 @endsection
-
-<div class="form-group col-sm-12">
-  <label for="patients_id">Patient Name: </label>
-  <select name="patients_id" id="patients_id" class="selectpicker" data-live-search="true">
-      <option value=""> Select Patient</option>
-    @foreach($patients as $patient)
-      <option value="{{$patient->id}}"> {{$patient->px_name}}</option>
-    @endforeach
-  </select>
-</div> 
-
+ 
 <!-- Con Height Field -->
 <div class="form-group col-sm-12">
     {!! Form::label('con_height', 'Height:') !!}
