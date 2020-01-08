639b474c8983db978e6dce5f5f4712d1e14d89df
emjay97 <mjayestilles@gmail.com>
2020-01-08 10:27:01 +0800

    Edited Users migration.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,13 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('role')->default(4);
+            $table->unsignedBigInteger('role_id')->index()->default(4);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->softDeletes();
             $table->timestamps();
+
         });
     }
 
